6dc16eae6ee67a52417f79005a4857a259de5bb6
LulDrako <karimfeki2004@gmail.com>
2024-08-22 06:56:13 +0100

    contact fix

--- my-vue-app/src/pages/Accueil.vue
+++ my-vue-app/src/pages/Accueil.vue
@@ -4,10 +4,10 @@
             <source src="../media/Only Basketball _ Nike.mp4" type="video/mp4">
         </video>
         <p>
-            Bienvenue chez Pete ta Paire, où style et performance se rencontrent pour créer une expérience inégalée.
+            Bienvenue chez CopUrShoe, où style et performance se rencontrent pour créer une expérience inégalée.
             De la rue à la piste, notre sélection de chaussures, vêtements et accessoires incarne l'esprit de l'audace et de l'innovation.
             <p>Rejoignez-nous dans notre quête pour repousser les limites et redéfinir le jeu.</p>
-        <p>Bienvenue dans la famille Pete ta Paire.</p>
+        <p>Bienvenue dans la famille CopUrShoe.</p>
         </p>
     </main>
 </template>
--- my-vue-app/src/pages/Contact.vue
+++ my-vue-app/src/pages/Contact.vue
@@ -3,16 +3,16 @@
         <h1>Contact</h1>
         <div class="contact-photos">
             <a href="https://www.linkedin.com/in/mohamed-karim-feki-18ab66249/">
-                <img src="https://media.licdn.com/dms/image/D4E35AQERucGERcc6fQ/profile-framedphoto-shrink_400_400/0/1708616595189?e=1710374400&v=beta&t=5_XlC1cqVwKc0oBE0vtoC9iX0hcq8n4F1xxf8w-i_WE" class="contact-image">
+                <img src="../media/karim.png" class="contact-image">
             </a>
             <a href="https://www.linkedin.com/in/rayan-toumert/">
-                <img src="https://media.licdn.com/dms/image/D4E35AQHS_CWSK4FaJA/profile-framedphoto-shrink_800_800/0/1707215678462?e=1710457200&v=beta&t=y6Pe7X_bdQaDlvmumZ2z673z7lng_aEKxbtcujLziR0" class="contact-image">
+                <img src="../media/rayan.png" class="contact-image">
             </a>
             <a href="https://www.linkedin.com/in/ily%C3%A8s-manai-a76649266/">
                 <img src="../media/ilyes.jpg" class="contact-image">
             </a>
             <a href="https://www.linkedin.com/in/ervin-goby-b52b25235/">
-                <img src="https://media.licdn.com/dms/image/D4E35AQHabs_X83QYCg/profile-framedphoto-shrink_100_100/0/1700214075352?e=1710457200&v=beta&t=t0wKwgbw5IicR81_XC9uAOU77NQ-4yOlttWraKKw0Wg" class="contact-image">
+                <img src="../media/ervin.png" class="contact-image">
             </a>
         </div>
     </main>
